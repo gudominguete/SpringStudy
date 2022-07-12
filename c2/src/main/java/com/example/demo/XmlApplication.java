68e38a4654762d0fe6c371ae8369cd54fec470e6
Gustavo Lopes <gudominguete@gmail.com>
2022-07-12 00:16:15 -0300

    XML with context

--- c2/src/main/java/com/example/demo/XmlApplication.java
+++ c2/src/main/java/com/example/demo/XmlApplication.java
@@ -2,18 +2,23 @@ package com.example.demo;
 
 import com.example.demo.basic.BinarySearchImpl;
 import com.example.demo.xml.XMLPersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configurable
-@ComponentScan
 public class XmlApplication {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(XmlApplication.class);
     public static void main(String[] args){
         try(ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")){
 
+            LOGGER.info("{}", (Object) applicationContext.getBeanDefinitionNames());
+
             XMLPersonDAO xmlPersonDAO =  applicationContext.getBean(XMLPersonDAO.class);
+
+
         }
     }
 }
