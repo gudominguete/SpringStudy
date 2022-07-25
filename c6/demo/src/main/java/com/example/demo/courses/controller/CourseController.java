d88c8e9cf9b5065e2cdda20055aed54b61de3687
Gustavo Lopes <gudominguete@gmail.com>
2022-07-24 22:58:09 -0300

    start web project

---
+++ c6/demo/src/main/java/com/example/demo/courses/controller/CourseController.java
@@ -0,0 +1,16 @@
+package com.example.demo.courses.controller;
+
+import com.example.demo.courses.bean.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+    @GetMapping("/courses")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(new Course(1L," Learn MicroServices", "In28minutes"));
+    }
+}
