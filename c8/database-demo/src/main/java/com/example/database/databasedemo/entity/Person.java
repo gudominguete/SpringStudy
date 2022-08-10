c5a9c7f539f229d65672a0d6d6a74b724e54e02a
Gustavo Lopes <gudominguete@gmail.com>
2022-08-10 00:02:36 -0300

    Chapter db

---
+++ c8/database-demo/src/main/java/com/example/database/databasedemo/entity/Person.java
@@ -0,0 +1,49 @@
+package com.example.database.databasedemo.entity;
+
+import java.util.Date;
+
+public class Person {
+    private int id;
+    private String name;
+    private String date;
+    private Date birthDate;
+
+    public Person(int id, String name, String date, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.birthDate = birthDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
